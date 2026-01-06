95e091e9e5cc86a6bf7d6284fd47312a2796b0f3
yowxy <iklilblender@gmail.com>
2026-01-06 20:44:40 +0700

    feat: setup framer motion and react-router

---
+++ aj-fe/src/auth/login/loginPage.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../../components/navbar";
+
+export default function LoginPage () {
+    return (
+        <>
+            <div>
+                <div>
+                    <Navbar />
+                </div>
+                <h1>ini halaman login</h1>
+            </div>
+        </>
+    )
+}
--- aj-fe/src/components/buttonCustom.tsx
+++ aj-fe/src/components/buttonCustom.tsx
@@ -1,7 +1,7 @@
 export default function ButtonCustom() {
     return (
         <button className="bg-gradient-to-r from-[#3A4CEC] to-[#A44CEE] hover:opacity-90 transition-opacity text-white font-bold py-3 px-12 rounded-2xl font-jakarta-sans text-2xl cursor-pointer h-[77px] w-[301px]">
-            Start Course
+            <a href="">  Start Course</a>
         </button>
     );
 }
